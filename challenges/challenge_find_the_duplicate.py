da2d8d03f760cc1bfc94e20e87f788d510f5e4d2
vinicius <vinicius.savordelli@gmail.com>
2022-08-31 18:46:02 -0300

    feat(challenge_find_the_duplicate)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    scanned_nums = []
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+        if num in scanned_nums:
+            return num
+        scanned_nums.append(num)
+    return False
