f46301827d00ec8c815bda401d8a9336432e5d84
vinicius <vinicius.savordelli@gmail.com>
2022-08-31 18:47:06 -0300

    refactor(challenge_find_the_duplicate) change variable type to a faster one

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,9 +1,9 @@
 def find_duplicate(nums):
-    scanned_nums = []
+    scanned_nums = {}
     for num in nums:
         if not isinstance(num, int) or num < 1:
             return False
         if num in scanned_nums:
             return num
-        scanned_nums.append(num)
+        scanned_nums[num] = True
     return False
