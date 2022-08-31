296ba7d9049591b6860e883d62fb0520f54106b4
vinicius <vinicius.savordelli@gmail.com>
2022-08-31 15:54:15 -0300

    feat(challenge_study_schedule)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    students_online = 0
+    if target_time is None:
+        return None
+    for startingTime, endingTime in permanence_period:
+        if not isinstance(startingTime, int) or not isinstance(
+            endingTime, int
+        ):
+            return None
+        if target_time >= startingTime and target_time <= endingTime:
+            students_online += 1
+    return students_online
