2e5d05aca2266dc21afa21e5336ac98c9ac8eaef
vinicius <vinicius.savordelli@gmail.com>
2022-08-31 18:01:44 -0300

    feat(challenge_anagrams)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
+def str_sort(str):
+    strLen = len(str)
+
+    for index in range(strLen - 1):
+        for search_index in range(strLen - 1 - index):
+            if str[search_index] > str[search_index + 1]:
+                current_element = str[search_index]
+                str[search_index] = str[search_index + 1]
+                str[search_index + 1] = current_element
+    return str
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    order_fisrt_string = str_sort(list(first_string.lower()))
+    order_second_string = str_sort(list(second_string.lower()))
+
+    if order_fisrt_string == order_second_string:
+        return True
+    return False
